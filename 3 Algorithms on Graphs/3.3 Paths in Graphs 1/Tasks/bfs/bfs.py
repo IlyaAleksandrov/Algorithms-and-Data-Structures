4dc108ce7dde28ce63c52589d8ccee3e11afa294
ilya Aleksandrov <aleksandroff7@yandex.ru>
2018-04-02 10:44:27 +0700

    1.1

---
+++ 3 Algorithms on Graphs/3.3 Paths in Graphs 1/Tasks/bfs/bfs.py
@@ -0,0 +1,50 @@
+# Problem Introduction
+# You would like to compute the minimum number of flight segments to get from one city to another one. For
+# this, you construct the following undirected graph: vertices represent cities, there is an edge between two
+# vertices whenever there is a flight between the corresponding two cities. Then, it suffices to find a shortest
+# path from one of the given cities to the other one.
+
+# Task. Given an undirected graph with n vertices and m edges and two vertices u and v, compute the length
+# of a shortest path between u and v (that is, the minimum number of edges in a path from u to v).
+
+# Input Format. A graph is given in the standard format. The next line contains two vertices u and v.
+
+# Constraints. 2  n  105, 0  m  105, u 6= v, 1  u; v  n.
+
+# Output Format. Output the minimum number of edges in a path from u to v, or 􀀀1 if there is no path.
+
+import sys
+import queue
+
+
+# implementation of Breadth-first Search
+# инициализируем массив расстояний значениями -1, от начального узла ищем все связанные узлы, присваиваем им
+# расстояние 1 и добавляем в очередь на обработку. Продолжаем поиск дочерних непосещеных узлов,
+# каждый раз увеличивая расстояние до него на 1 от родительского.
+def distance(adj, s, t):
+    Q.append(s)
+    dist[s] = 0
+    while Q != []:
+        x = Q.pop(0)
+        for i in adj[x]:
+            if dist[i] == -1:
+                Q.append(i)
+                dist[i] = dist[x] + 1
+    return dist[t]
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n, m = data[0:2]
+    data = data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    adj = [[] for _ in range(n)]
+    dist = [-1 for i in range(n)]
+    Q = []
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+        adj[b - 1].append(a - 1)
+    s, t = data[2 * m] - 1, data[2 * m + 1] - 1
+    print(distance(adj, s, t))
+
